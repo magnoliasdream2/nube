4dcc679321fcd260387679f6c9b712722f810871
magnoliasdream <magnolias.93@gmail.com>
2016-11-29 10:42:34 -0800

    Archivos static

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -104,7 +104,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/' 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 MEDIA_URL = 'http://127.0.0.1:8000/media/'
